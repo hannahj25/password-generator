cb955b304139ceaf6d7390f10f75b9ef661b9cf0
Hannah <94648556+hannahj25@users.noreply.github.com>
2021-12-25 03:29:53 +1030

    added comments to javascript file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// Assignment Code
+// Assigns variable to button using id
 var generateBtn = document.querySelector("#generate");
 
+//Declares strings of characters available to use in password
 const charTypes =  {
 lowerCase: "abcdefghijklmnopqrstuvwxyz",
 upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -9,16 +10,18 @@ special: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 }
 
 
-// Write password to the #password input
+// Generates password
 function generatePassword() {
   var passwordText = document.querySelector("#password");
   var selectedChar = "";
 
+  // Ascertains user's desired password length
   var length = window.prompt("Please enter desired password length (from 8 to 128 characters).");
   while (length < 8 || length > 128) {
     length = window.prompt("Password length must be between 8-128 characters.");
   }
 
+  // Allows user to select which characters to include in password
   var useLower = window.confirm("Include lowercase?");
   var useUpper = window.confirm("Include uppercase?");
   var useNum = window.confirm("Include numbers?");
@@ -41,20 +44,16 @@ function generatePassword() {
     selectedChar += charTypes.special;
   }
   
-
+  // Creates password with the user's chosen criteria
   var password = "";
   for (i = 0; i < length; i++) {
     password += selectedChar[Math.floor(Math.random() * selectedChar.length)]
   }
-
+  
+  // Displays generated password to page
   passwordText.textContent = password;
 
 }
 
-
-
-
-
-
-// Add event listener to generate button
+// Adds event listener to generate button, activating the function to generate a password
 generateBtn.addEventListener("click", generatePassword);
